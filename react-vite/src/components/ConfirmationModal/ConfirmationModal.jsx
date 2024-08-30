4a8d18a558145d68faae0ebba8652b773d150fab
Mengxuan Liang <mengxuanliang007@gmail.com>
2024-08-30 19:36:13 -0400

    added styling

--- react-vite/src/components/ConfirmationModal/ConfirmationModal.jsx
+++ react-vite/src/components/ConfirmationModal/ConfirmationModal.jsx
@@ -10,8 +10,10 @@ function ConfirmationModal({ show, onClose, onConfirm, message }) {
     <div className="modal-overlay">
       <div className="modal-content">
         <p>{message}</p>
+        <br></br>
         <div className="modal-buttons">
           <button onClick={onConfirm}>Yes</button>
+          {' '}
           <button onClick={onClose}>No</button>
         </div>
       </div>
