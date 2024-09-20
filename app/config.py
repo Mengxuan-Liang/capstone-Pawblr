65f4899d9fb829b78fe3a59b55a54f58c76f4e98
Mengxuan Liang <mengxuanliang007@gmail.com>
2024-09-20 15:47:31 -0400

    added config

--- app/config.py
+++ app/config.py
@@ -15,4 +15,5 @@ class Config:
     S3_BUCKET = os.environ.get('S3_BUCKET')
     S3_KEY = os.environ.get('S3_KEY')
     S3_SECRET = os.environ.get('S3_SECRET')
-
+    SOCKET_SERVER_URL = os.environ.get(
+        'DATABASE_URL').replace('postgres://', 'postgresql://')
