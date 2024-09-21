07b6545636bf832140828ee71ac87e37b8d9394d
Mengxuan Liang <mengxuanliang007@gmail.com>
2024-09-21 12:22:27 -0400

    fixed message table

--- app/models/message.py
+++ app/models/message.py
@@ -2,6 +2,7 @@ from .db import db, environment,SCHEMA, add_prefix_for_prod
 from .user import User
 
 class Message(db.Model):
+    __tablename__ = 'message'
     id = db.Column(db.Integer, primary_key=True)
     sender_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
     recipient_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
--- migrations/versions/20240921_122147_initial_migration.py
+++ migrations/versions/20240921_122147_initial_migration.py
@@ -1,8 +1,8 @@
 """initial migration
 
-Revision ID: 2a537302b964
+Revision ID: e4d2a2bd9e91
 Revises: 
-Create Date: 2024-09-21 11:24:32.066218
+Create Date: 2024-09-21 12:21:47.630841
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2a537302b964'
+revision = 'e4d2a2bd9e91'
 down_revision = None
 branch_labels = None
 depends_on = None
